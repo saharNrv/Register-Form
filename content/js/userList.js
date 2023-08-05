16b802bdaf684019f74006c155fceb54261434b5
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-08-05 20:38:02 +0330

    add yes and no funcs for accept and reject user

--- content/js/userList.js
+++ content/js/userList.js
@@ -1,12 +1,20 @@
-window.addEventListener('load',()=>{
-    const userListWrapper=document.querySelector('.user-list-wrapper')
+const modal = document.querySelector('.modal')
+const userListWrapper = document.querySelector('.user-list-wrapper')
+let userID = null;
+window.addEventListener('load', () => {
+
+    getUSerList()
+
+})
+
+function getUSerList() {
 
     fetch('http://localhost:3000/api/user-list/')
-    .then(res=>res.json())
-    .then(data=>{
-            userListWrapper.innerHTML=''
+        .then(res => res.json())
+        .then(data => {
+            userListWrapper.innerHTML = ''
             data.forEach(user => {
-                userListWrapper.insertAdjacentHTML('beforeend',`
+                userListWrapper.insertAdjacentHTML('beforeend', `
                 <div class="user-info">
                 <div class="user-info-right">
                     <img src="./content/img/download.png" alt="user image" class="user-img">
@@ -19,25 +27,35 @@ window.addEventListener('load',()=>{
                     <h3>${user.username}</h3>
                 </div>
                 <div class="user-remove">
-                    <button class="delete-icon" onClick="removeUser('${user.id}')"><i class="fa-solid fa-trash"></i></button>
+                    <button class="delete-icon" onClick="showremoveUserModal('${user.id}')"><i class="fa-solid fa-trash"></i></button>
                     <button class="edit-icon"><i class="fa-solid fa-pen-to-square"></i></button>
                 </div>
             </div>`)
-                
+
             });
-           
+
+        })
+}
+
+function showremoveUserModal(id) {
+
+    userID = id
+    modal.classList.add('active')
+
+
+}
+function noBtn() {
+    modal.classList.remove('active')
+}
+function yesBtn() {
+
+    fetch(`http://localhost:3000/api/user-list/${userID}`, {
+        method: "DELETE"
+    }).then(res => res.json)
+        .then(data => {
+            console.log(data);
+            noBtn()
+            getUSerList()
         })
 
-
-})
-
-function removeUser(id){
-    console.log(id);
-
-    fetch(`http://localhost:3000/api/user-list/${id}`,{
-        method:"DELETE"
-    }).then(res=>res.json)
-      .then(data=>{
-        console.log(data);
-      })
 }
