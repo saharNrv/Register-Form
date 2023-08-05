e6acd5bc66bb15de8c79d67591496bc686622d60
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-08-05 23:29:51 +0330

    create edit modal for userList

--- content/js/userList.js
+++ content/js/userList.js
@@ -1,5 +1,13 @@
 const modal = document.querySelector('.modal')
+const editModal = document.querySelector('.edit-modal')
 const userListWrapper = document.querySelector('.user-list-wrapper')
+const none = document.querySelector('.none')
+const firstnameInput = document.querySelector('.firstname')
+const lastnameInput = document.querySelector('.lastname')
+const usernameInput = document.querySelector('.username')
+const passwordInput = document.querySelector('.password')
+const editBtn = document.querySelector('.editBtn')
+const editClose = document.querySelector('.editClose')
 let userID = null;
 window.addEventListener('load', () => {
 
@@ -12,6 +20,11 @@ function getUSerList() {
     fetch('http://localhost:3000/api/user-list/')
         .then(res => res.json())
         .then(data => {
+            console.log(data.length);
+            if (data.length === 0) {
+                none.style.display = 'block'
+
+            }
             userListWrapper.innerHTML = ''
             data.forEach(user => {
                 userListWrapper.insertAdjacentHTML('beforeend', `
@@ -28,7 +41,7 @@ function getUSerList() {
                 </div>
                 <div class="user-remove">
                     <button class="delete-icon" onClick="showremoveUserModal('${user.id}')"><i class="fa-solid fa-trash"></i></button>
-                    <button class="edit-icon"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="edit-icon" onClick=showEditModal(${JSON.stringify(user)})><i class="fa-solid fa-pen-to-square"></i></button>
                 </div>
             </div>`)
 
@@ -59,3 +72,46 @@ function yesBtn() {
         })
 
 }
+
+function showEditModal(user) {
+    userID = user.id
+    firstnameInput.value=user.firstname;
+    lastnameInput.value=user.lastname;
+    usernameInput.value=user.username;
+    passwordInput.value=user.password;
+    editModal.classList.add('active')
+
+}
+
+
+
+editBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    let editUser = {
+        firstname: firstnameInput.value,
+        lastname: lastnameInput.value,
+        username: usernameInput.value,
+        password: passwordInput.value,
+    }
+    fetch(`http://localhost:3000/api/user-list/${userID}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editUser)
+    }).then(res => res.json())
+        .then(data => {
+           
+            getUSerList()
+        })
+
+
+    editModal.classList.remove('active')
+
+})
+
+editClose.addEventListener('click',()=>{
+    editModal.classList.remove('active')
+
+})
